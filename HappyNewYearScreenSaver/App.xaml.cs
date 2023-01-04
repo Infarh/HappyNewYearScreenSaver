a04ad34fed0a47b314cee40b041922b726b914b4
Infarh <shmachilin@yandex.ru>
2023-01-04 15:29:23 +0300

    Обработка ключей командной строки

--- HappyNewYearScreenSaver/App.xaml.cs
+++ HappyNewYearScreenSaver/App.xaml.cs
@@ -1,6 +1,65 @@
-﻿namespace HappyNewYearScreenSaver;
+﻿using System.Windows;
+
+namespace HappyNewYearScreenSaver;
 
 public partial class App
 {
+    private enum Mode
+    {
+        Configuration,
+        FullScreen,
+        Preview
+    }
 
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var mode = Mode.Configuration;
+
+        nint parent_handle = default;
+
+        //var args = Environment.GetCommandLineArgs();
+        var args = e.Args;
+        var args_count = args.Length;
+        for (var i = 0; i < args_count; i++)
+        {
+            var arg = args[i].AsSpan();
+            if (arg.StartsWith("/c", StringComparison.OrdinalIgnoreCase))
+            {
+                mode = Mode.Configuration;
+
+                if (arg.IndexOf(':') is > 0 and var s_index && nint.TryParse(arg[(s_index + 1)..], out var s_handle))
+                    parent_handle = s_handle;
+                else if (i + 1 < args_count && nint.TryParse(args[i + 1], out var p_handle))
+                    parent_handle = p_handle;
+
+            }
+            else if (arg.StartsWith("/p", StringComparison.OrdinalIgnoreCase))
+            {
+                mode = Mode.Preview;
+
+                if (arg.IndexOf(':') is > 0 and var s_index && nint.TryParse(arg[(s_index + 1)..], out var s_handle))
+                    parent_handle = s_handle;
+                else if (i + 1 < args_count && nint.TryParse(args[i + 1], out var p_handle))
+                    parent_handle = p_handle;
+            }
+            else if (arg.Equals("/s", StringComparison.OrdinalIgnoreCase))
+                mode = Mode.FullScreen;
+
+            switch (mode)
+            {
+                case Mode.Configuration:
+                    MessageBox.Show("Конфигурация...");
+                    Shutdown();
+                    return;
+
+                case Mode.Preview: 
+                    break;
+
+                case Mode.FullScreen: 
+                    break;
+            }
+        }
+    }
 }
